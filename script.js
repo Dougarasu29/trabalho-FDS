ee327e8bff31c314b9eed7dbad67e0bf8d44f948
Douglas Lima <douglaslima1996@hotmail.com>
2025-07-04 23:48:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+    document.getElementById("meubotao").onclick = function() {
+      document.getElementById("mensagem").innerHTML = "“Você não precisa ver a escada inteira, apenas dê o primeiro passo.” – Martin Luther King Jr.";
+
+
+
+    setTimeout(function() {
+        document.getElementById("mensagem").innerHTML = "";
+      }, 5000);
+    };
+
+
